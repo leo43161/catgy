9fab5ff49354f92915e340ebd29af59589066fa2
Leonardo Palavecino <64588981+leo43161@users.noreply.github.com>
2024-11-12 15:18:01 -0300

    Se creo la seccion de categorias, con sus apis, su dialog y las cards

--- src/pages/dashboard/Products.jsx
+++ src/pages/dashboard/Products.jsx
@@ -1,11 +1,8 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Products } from "../../components/Products/Products"
-import { useState } from "react"
 import { Category } from "../../components/Category/Category"
 
 export default function Dashboard() {
-  const [products, setProducts] = useState([])
-  const [categories, setCategories] = useState([])
   return (
     <div className="container mx-auto p-6 min-h-screen">
       <h1 className="text-3xl font-bold mb-8 text-center">
@@ -21,11 +18,7 @@ export default function Dashboard() {
             <Products />
           </TabsContent>
           <TabsContent className="w-full" value="password">
-            <Category
-              products={products}
-              setCategories={setCategories}
-              categories={categories}
-            />
+            <Category/>
           </TabsContent>
         </Tabs>
       </div>
