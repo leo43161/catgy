e92e966a9012b38d5cd3eee9f9a96a6b7df8d21d
Leonardo Palavecino <64588981+leo43161@users.noreply.github.com>
2024-11-21 09:31:18 -0300

    Se implemento una nueva forma de loguear para ver si se puede solucionar un error en produccion

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,6 +6,10 @@ export default function Home() {
     return (
         <div>
             <h1>Bienvenidos</h1>
+            <span className="hidden">{process.env.NEXT_PUBLIC_MONGO_URI}</span>
+            <span className="hidden">{process.env.NEXT_PUBLIC_SECRET_JWT}</span>
+            <span className="hidden">{process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID}</span>
+            <span className="hidden">{process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY}</span>
         </div>
     );
 }
