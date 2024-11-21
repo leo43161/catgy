4cf094ddf94e8db2d2e441ab81742e178b55cd37
Leonardo Palavecino <64588981+leo43161@users.noreply.github.com>
2024-11-21 11:11:11 -0300

    Se implemento una nueva forma de llamar a los productos para ver si se puede solucionar un error en produccion

---
+++ src/pages/api/prueba.js
@@ -0,0 +1,64 @@
+import connectMongo from "@/lib/mongodb";
+import Product from "@/models/Product";
+
+export default async function handler(req, res) {
+  try {
+    console.log("CONNECTING TO MONGO");
+    await connectMongo();
+    console.log("CONNECTED TO MONGO");
+
+    if (req.method === "GET") {
+      console.log("FETCHING DOCUMENTS");
+      const { limit, offset, search } = req.query;
+      const query = search
+        ? { name: { $regex: search, $options: "i" }, active: true }
+        : { active: true };
+      const totalProducts = await Product.countDocuments(query);
+      const fetchedProducts = await Product.find(query)
+        .populate("categoryIDs", "name")
+        .skip(parseInt(offset) || 0)
+        .limit(parseInt(limit) || 10)
+        .exec();
+      return res.status(200).json({ products: fetchedProducts, total: totalProducts });
+    }
+
+    if (req.method === "POST") {
+      console.log("CREATING PRODUCT");
+      const createdProduct = await Product.create(req.body);
+      return res.status(201).json({
+        success: true,
+        message: "Product created successfully",
+        createdProduct,
+      });
+    }
+
+    if (req.method === "PUT") {
+      const { state, id, value } = req.query;
+      if (state && id && value) {
+        const updatedProduct = await Product.findByIdAndUpdate(id, { $set: { [state]: value } }, { new: true });
+        return res.status(200).json({
+          success: true,
+          message: "Product updated successfully",
+          updatedProduct,
+        });
+      } else {
+        return res.status(400).json({
+          success: false,
+          message: "Missing query parameters for update.",
+        });
+      }
+    }
+
+    res.status(405).json({
+      success: false,
+      message: `Method ${req.method} not allowed.`,
+    });
+  } catch (error) {
+    console.error("SERVER ERROR:", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error.",
+      error: error.message || "Unknown error.",
+    });
+  }
+}
